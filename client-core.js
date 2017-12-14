0457e3c957cbe140e7f67ff0b0ace8608a98f0c5
Albina-G <erfolg1996@mail.ru>
2017-12-14 22:41:49 +0500

    Моё решение задачи

--- client-core.js
+++ client-core.js
@@ -3,9 +3,108 @@
 module.exports.execute = execute;
 module.exports.isStar = true;
 
+const request = require('request');
+const url = 'http://localhost:8080/messages';
+const chalk = require('chalk');
+const red = chalk.hex('#f00');
+const green = chalk.hex('#0f0');
+
 function execute() {
     // Внутри этой функции нужно получить и обработать аргументы командной строки
     const args = process.argv;
+    let message = {};
+    switch (args[2]) {
+        case 'list':
+            message = parseMessage(args);
+            let answer = newRequest(createQuery(message, 'get'), 'get');
 
-    return Promise.resolve('Это строка будет выведена на консоль');
+            return Promise.resolve(answer);
+        case 'send':
+            message = parseMessage(args);
+            let requestMessage = newRequest(createQuery(message, 'post'), 'post');
+
+            return Promise.resolve(requestMessage);
+        default:
+            break;
+    }
+}
+
+function parseMessage(args) {
+    let message = {};
+    for (let i = 3; i < args.length; i++) {
+        let arg = args[i].match(/[^-].*/);
+        if (arg[0].indexOf('=') !== -1) {
+            let splitArg = arg[0].split('=');
+            message[splitArg[0]] = splitArg[1];
+        } else {
+            message[arg[0]] = args[i + 1];
+            i++;
+        }
+    }
+
+    return message;
+}
+
+function createQuery(message, method) {
+    if (!message.text && method === 'post') {
+        return;
+    }
+    let urlQuery = url;
+    let keysMessage = Object.keys(message);
+    if (keysMessage.length) {
+        urlQuery += '?';
+        keysMessage.forEach(item => {
+            urlQuery += item + '=' + message[item] + '&';
+        });
+        urlQuery = urlQuery.slice(0, -1);
+    }
+    const query = {
+        method: method,
+        url: urlQuery,
+        json: true
+    };
+    if (method === 'post') {
+        query.body = { text: message.text };
+    }
+
+    return query;
+}
+
+function newRequest(query, method) {
+
+    return new Promise((resolve) => {
+        request(query, function (err, res, body) {
+            if (err) {
+
+                return resolve(err);
+            }
+
+            return resolve(body);
+        });
+    })
+        .then(message => formRequest(message, method));
+}
+
+function formRequest(requestMessage, method) {
+    if (method === 'get') {
+        let finishGet = '';
+        for (let index = 0; index < requestMessage.length - 1; index++) {
+            finishGet += formRequest(requestMessage[index], 'post') + '\n\n';
+        }
+        finishGet += formRequest(requestMessage[requestMessage.length - 1], 'post');
+
+        return finishGet;
+    }
+    let finish = '';
+    if (requestMessage.hasOwnProperty('from')) {
+        finish += red('FROM') + ': ' + requestMessage.from + '\n';
+    }
+    if (requestMessage.hasOwnProperty('to')) {
+        finish += red('TO') + ': ' + requestMessage.to + '\n';
+    }
+    if (requestMessage.hasOwnProperty('text')) {
+        finish += green('TEXT') + ': ' + requestMessage.text;
+    }
+
+    return finish;
 }
--- server-core.js
+++ server-core.js
@@ -1,12 +1,71 @@
 'use strict';
 
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer();
+let allMessages = [];
 
 server.on('request', (req, res) => {
-    // Тут нужно обработать запрос
-    res.end();
+    let parseUrl = url.parse(req.url, true);
+    if (/^\/messages$/.test(parseUrl.pathname)) {
+        let textMessage = '';
+        switch (req.method) {
+            case 'GET':
+                res.end(createAnswerGet(parseUrl.query));
+                break;
+            case 'POST':
+                req.on('readable', () => {
+                    let reqRead = req.read();
+                    if (reqRead !== null) {
+                        textMessage += reqRead;
+                    }
+                });
+                req.on('end', () => {
+                    textMessage = JSON.parse(textMessage);
+                    textMessage = createAnswerPost(parseUrl.query, textMessage.text);
+                    res.end(JSON.stringify(textMessage));
+                });
+                break;
+            default:
+                res.statusCode = 404;
+                res.end();
+                break;
+        }
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
 });
 
 module.exports = server;
+
+function createAnswerPost(query, text) {
+    let answer = {};
+    Object.keys(query).forEach(key => {
+        answer[key] = query[key];
+    });
+    answer.text = text;
+    allMessages.push(answer);
+
+    return answer;
+}
+
+function createAnswerGet(query) {
+    if (!query.from && !query.to) {
+        return JSON.stringify(allMessages);
+    }
+
+    return JSON.stringify(allMessages.filter(message => {
+        if (!query.from) {
+
+            return message.to === query.to;
+        }
+        if (!query.to) {
+
+            return message.from === query.from;
+        }
+
+        return message.from === query.from && message.to === query.to;
+    }));
+}
